267f552eea4e4ad2a97c545f19ba40c1a24defdf
Pear <flippyloveskelly2@gmail.com>
2023-03-17 15:00:46 -0600

    Update version number

--- GParamStudio.cs
+++ GParamStudio.cs
@@ -10,7 +10,7 @@ namespace GParamStudio;
 
 public partial class GParamStudio : Form
 {
-    private const string version = "1.08";
+    private const string version = "1.09";
     private static GPARAM gparam = new();
     private static TreeNode? prevSelectedNode;
     private static string gparamFileName = "";
